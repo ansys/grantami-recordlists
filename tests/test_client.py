d8bd11260a9e115dec7ddf3c06893a2f19e8d4dd
Ludovic Steinbach <ludovic.steinbach@ansys.com>
2023-02-10 15:48:49 +0100

    Add unit tests

---
+++ tests/test_client.py
@@ -0,0 +1,144 @@
+from typing import Type, Any
+from unittest.mock import Mock
+
+import pytest
+
+from ansys.grantami.recordlists._connection import RecordListApiClient
+from ansys.grantami.recordlists.models import RecordListItem
+from ansys.grantami.serverapi_openapi.api import ListManagementApi, ListItemApi, ListPermissionsApi
+from ansys.grantami.serverapi_openapi.models import (
+    GrantaServerApiListsDtoRecordListHeader,
+    GrantaServerApiListsDtoRecordListItems,
+    GrantaServerApiListsDtoListItem,
+)
+
+
+@pytest.fixture
+def client():
+    client = RecordListApiClient(Mock(), Mock(), Mock())
+    return client
+
+
+class TestClientMethod:
+    _return_value: Any
+    _api: Type
+    _api_method: str
+
+    @pytest.fixture
+    def api_method(self, monkeypatch):
+        mocked_method = Mock(return_value=self._return_value)
+        monkeypatch.setattr(
+            self._api, self._api_method, mocked_method
+        )
+        return mocked_method
+
+
+class TestReadList(TestClientMethod):
+    _return_value = Mock(spec=GrantaServerApiListsDtoRecordListHeader)
+    _api = ListManagementApi
+    _api_method = "api_v1_lists_list_list_identifier_get"
+
+    def test_read_list(self, client, api_method):
+        identifier = "00000-0000a"
+        client.get_list(identifier)
+        api_method.assert_called_once_with(identifier)
+
+    # TODO test variations of invalid identifiers
+
+
+class TestReadAllLists(TestClientMethod):
+    _return_value = []
+    _api = ListManagementApi
+    _api_method = "api_v1_lists_get"
+
+    def test_read_all_lists(self, client, api_method):
+        lists = client.get_all_lists()
+        api_method.assert_called_once_with()
+        assert lists == []
+
+    # TODO test with return value
+
+
+class TestReadItems(TestClientMethod):
+    r_value = Mock(spec=GrantaServerApiListsDtoRecordListItems)
+    r_value.items = []
+    _return_value = r_value
+    _api = ListItemApi
+    _api_method = "api_v1_lists_list_list_identifier_items_get"
+
+    def test_read_items(self, client, api_method):
+        identifier = "00000-0000a"
+        items = client.get_list_items(identifier)
+
+        api_method.assert_called_once_with(identifier)
+        assert items == []
+
+    # TODO test with return value
+
+
+class TestAddItems(TestClientMethod):
+    _return_value = None
+    _api = ListItemApi
+    _api_method = "api_v1_lists_list_list_identifier_items_add_post"
+
+    @pytest.mark.parametrize("items", [
+        None, [], set()
+    ])
+    def test_add_no_items(self, client, api_method, items):
+        identifier = "00000-0000a"
+        response = client.add_items_to_list(identifier, items)
+
+        assert response is None
+        api_method.assert_not_called()
+
+    def test_add_items(self, client, api_method):
+        identifier = "00000-0000a"
+        items = [RecordListItem("a", "b", "c")]
+        expected_body = GrantaServerApiListsDtoRecordListItems(
+                items=[
+                    GrantaServerApiListsDtoListItem(
+                        database_guid="a",
+                        table_guid="b",
+                        record_history_guid="c",
+                    )
+                ]
+            )
+
+        response = client.add_items_to_list(identifier, items)
+
+        assert response is None
+        api_method.assert_called_once_with(identifier, body=expected_body)
+
+
+class TestRemoveItems(TestClientMethod):
+    _return_value = None
+    _api = ListItemApi
+    _api_method = "api_v1_lists_list_list_identifier_items_remove_post"
+
+    @pytest.mark.parametrize("items", [
+        None, [], set()
+    ])
+    def test_remove_no_items(self, client, api_method, items):
+        identifier = "00000-0000a"
+        response = client.remove_items_from_list(identifier, items)
+
+        assert response is None
+        api_method.assert_not_called()
+
+    def test_add_items(self, client, api_method):
+        identifier = "00000-0000a"
+        items = [RecordListItem("a", "b", "c")]
+        expected_body = GrantaServerApiListsDtoRecordListItems(
+                items=[
+                    GrantaServerApiListsDtoListItem(
+                        database_guid="a",
+                        table_guid="b",
+                        record_history_guid="c",
+                    )
+                ]
+            )
+
+        response = client.remove_items_from_list(identifier, items)
+
+        assert response is None
+        api_method.assert_called_once_with(identifier, body=expected_body)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,50 @@
-from ansys.grantami.serverapi_openapi import models
+from datetime import datetime
+from unittest.mock import Mock
+
+import pytest
+
+from ansys.grantami.recordlists._connection import RecordListApiClient
+from ansys.grantami.recordlists.models import RecordList, RecordListItem
 
 
-def test_instantiate_model():
-    model = models.GrantaServerApiListsDtoRecordListCreate(name="Test")
+class TestRecordList:
+
+    @pytest.fixture
+    def mock_client(self):
+        return Mock(spec=RecordListApiClient)
+
+    @pytest.fixture
+    def record_list(self, mock_client):
+        return RecordList(
+            mock_client,
+            identifier="00000",
+            name="UnitTestList",
+            created_timestamp=datetime.now(),
+            published=False,
+            awaiting_approval=False,
+        )
+
+    def test_read_items(self, mock_client, record_list):
+        record_list.read_items()
+        assert mock_client.get_list_items.called_once_with("00000")
+
+    items_variations = pytest.mark.parametrize(
+        "items",
+        [
+            [],
+            None,
+            ["1", 2],
+            [RecordListItem("db", "table", "record")]
+        ]
+    )
+
+    @items_variations
+    def test_add_items(self, mock_client, record_list, items):
+        record_list.add_items(items)
+        assert mock_client.add_items_to_list.called_once_with(items)
+
+    @items_variations
+    def test_remove_items(self, mock_client, record_list, items):
+        record_list.add_items([])
+        assert mock_client.add_items_to_list.called_once_with([])
+
