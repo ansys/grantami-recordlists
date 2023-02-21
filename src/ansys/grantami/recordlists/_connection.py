8b4a4f6f2820e6712b624c1d722baf3473a4d1a2
Ludovic Steinbach <ludovic.steinbach@ansys.com>
2023-02-21 10:42:05 +0100

    Change URL handling

--- src/ansys/grantami/recordlists/_connection.py
+++ src/ansys/grantami/recordlists/_connection.py
@@ -11,8 +11,8 @@ from ansys.openapi.common import (
 from ansys.grantami.recordlists.models import RecordList, RecordListItem
 from ansys.grantami.recordlists._utils import _ArgNotProvided
 
-
-AUTH_PATH = "/swagger/v1/swagger.json"
+PROXY_PATH = "/proxy/v1.svc"
+AUTH_PATH = "/Health/v2.svc"
 
 
 class RecordListApiClient(ApiClient):
@@ -201,10 +201,12 @@ class Connection(ApiClientFactory):
     Connects to a Granta MI ServerAPI instance.
     """
 
-    def __init__(self, api_url: str, session_configuration: Optional[SessionConfiguration] = None):
-        auth_url = api_url.strip("/") + AUTH_PATH
+    def __init__(
+        self, servicelayer_url: str, session_configuration: Optional[SessionConfiguration] = None
+    ):
+        auth_url = servicelayer_url.strip("/") + AUTH_PATH
         super().__init__(auth_url, session_configuration)
-        self._base_server_api_url = api_url
+        self._base_service_layer_url = servicelayer_url
 
     def connect(self) -> RecordListApiClient:
         """
@@ -215,7 +217,7 @@ class Connection(ApiClientFactory):
         self._validate_builder()
         client = RecordListApiClient(
             self._session,
-            self._base_server_api_url,
+            self._base_service_layer_url + PROXY_PATH,
             self._session_configuration,
         )
         client.setup_client(models)
