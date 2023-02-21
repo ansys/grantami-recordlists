e45d8d0dac4d7ab73944a4c16ee8aed7db207b5f
Ludovic Steinbach <ludovic.steinbach@ansys.com>
2023-02-21 15:54:06 +0100

    Don't ignore ipython settings

---
+++ .ipython/profile_default/startup/10-monkeypatch-connection.py
@@ -0,0 +1,42 @@
+import os
+from ansys.grantami.recordlists import Connection
+from ansys.grantami.recordlists._connection import RecordListApiClient
+
+# Monkey patch the Connection() class to use the environment variable-specified server URL.
+original_ctor = Connection.__init__
+
+
+def new_init(self: Connection, _):
+    original_ctor(self, os.getenv("TEST_SL_URL"))
+
+
+Connection.__init__ = new_init
+
+# Monkey patch the Connection builder methods to use the environment variable-specified credentials.
+Connection.with_creds_original = Connection.with_credentials
+
+
+def with_credentials(self: Connection, _, __) -> Connection:
+    user_name = os.getenv("TEST_USER")
+    password = os.getenv("TEST_PASS")
+    return self.with_creds_original(user_name, password)
+
+
+def with_autologon(self: Connection) -> Connection:
+    return self.with_credentials("foo", "bar")
+
+
+Connection.with_credentials = with_credentials
+Connection.with_autologon = with_autologon
+
+
+# Monkey patch the Client object to report the url specified in the code, or the one below if not
+# overridden
+server_url = "http://my_grantami_server/mi_servicelayer"
+
+
+def __repr__(self: RecordListApiClient) -> str:
+    return f'<RecordListApiClient: url="{server_url}", dbkey="{self._db_key}">'
+
+
+RecordListApiClient.__repr__ = __repr__
