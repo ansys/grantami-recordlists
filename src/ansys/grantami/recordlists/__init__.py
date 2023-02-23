5341dc2b9a5394251da7abedf5edc79c6350fc0e
Ludovic Steinbach <ludovic.steinbach@ansys.com>
2023-02-23 18:50:55 +0100

    Drop support for Python 3.7

--- src/ansys/grantami/recordlists/__init__.py
+++ src/ansys/grantami/recordlists/__init__.py
@@ -2,9 +2,6 @@
 from ._connection import Connection
 from .models import RecordList
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
+import importlib.metadata as importlib_metadata
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
