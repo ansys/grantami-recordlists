757acb7309873a53000275db3e9bbf1b8a03f1d7
Ludovic Steinbach <ludovic.steinbach@ansys.com>
2023-10-10 11:36:30 +0200

    Re-enable SSL verification

--- .github/scripts/check_server.py
+++ .github/scripts/check_server.py
@@ -39,7 +39,6 @@ def check_status(url: str, auth_header: HTTPBasicAuth) -> bool:
         response = requests.get(
             url + "/Health/v2.svc/",
             auth=auth_header,
-            verify=False,
             headers={
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0"
             },
